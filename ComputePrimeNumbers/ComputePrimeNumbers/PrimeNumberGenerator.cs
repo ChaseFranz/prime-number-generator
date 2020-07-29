8d8c46f11de5ca5b16a8f11843b89af90ea53a6d
Chase Franz <chase@chasefranz.com>
2020-07-29 12:26:56 -0500

    Console application that generates and logs prime numbers using parallel computation

---
+++ ComputePrimeNumbers/ComputePrimeNumbers/PrimeNumberGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputePrimeNumbers
+{
+    public class PrimeNumberGenerator
+    {
+        public ConcurrentBag<int> PrimeNumbers { get; private set; }
+
+        /// <summary>
+        /// Generates all prime numbers up to N.
+        /// </summary>
+        /// <returns></returns>
+        public void CalculatePrimes(int n)
+        {
+            PrimeNumbers = new ConcurrentBag<int>();
+            int minPrimeCandidate = 2;
+
+            Parallel.For(minPrimeCandidate, n + 1, (int primeCandidate) =>
+            {
+                bool isPrime = true;
+                double primeCandidateRoot = Math.Sqrt(primeCandidate);
+
+                // check current primeCandidate for prime
+                for (int divisor = 2; divisor <= primeCandidateRoot; divisor++)
+                {
+                    if (primeCandidate % divisor == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    PrimeNumbers.Add(primeCandidate);
+                }
+            });
+        }
+
+        public void LogPrimeNumbers()
+        {
+            if (PrimeNumbers.IsEmpty)
+            {
+                Console.WriteLine($"No prime numbers");
+            }
+            else
+            {
+                Console.WriteLine("Prime Numbers:");
+                foreach (int prime in PrimeNumbers.OrderBy(p => p).ToList())
+                {
+                    Console.Write($"{prime} ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ ComputePrimeNumbers/ComputePrimeNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ComputePrimeNumbers
+{
+    class Program
+    {
+        private static PrimeNumberGenerator primeNumberGenerator;
+        private static readonly int EXIT_CODE = -1;
+
+        public static void Main(string[] args)
+        {
+            primeNumberGenerator = new PrimeNumberGenerator();
+
+            bool continueLoop = true;
+            do
+            {
+                Console.WriteLine("Calculate Primes from 2 to N.");
+                Console.WriteLine("Please enter a Natural Number or -1 to exit.");
+
+                bool goodInput = int.TryParse(Console.ReadLine(), out int n);
+
+                if (goodInput && n >= 2)
+                {
+                    Stopwatch stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                    primeNumberGenerator.CalculatePrimes(n);
+                    stopWatch.Stop();
+                    Console.WriteLine($"Execution Time: {stopWatch.Elapsed.TotalSeconds} seconds");
+
+                    AskUserToPrintPrimes();
+                }
+                else if (n == EXIT_CODE)
+                {
+                    continueLoop = false;
+                }
+            } while (continueLoop);
+        }
+
+        private static void AskUserToPrintPrimes()
+        {
+            Console.WriteLine("Print Prime Numbers to Console? (y/n)");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "y")
+            {
+                primeNumberGenerator?.LogPrimeNumbers();
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
